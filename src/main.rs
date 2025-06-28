ccc149d145043e8c7e08d9cd2e450678ff5f141b
Durga Prasad <durgaprasadachana1108@gmail.com>
2025-06-29 03:18:00 +0530

    Feat:update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
-use rand::prelude::*;
+//use rand::prelude::*;
+//fn main() {
+//    let mut rng = rand::rng();
+//    println!("Random die roll: {}", rng.random_range(1..=10));
+//}
+//
+//fn add(a: u32, b: u32) {
+//    println!("{}", a);
+//}
+//
+use std::fs::File;
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}
 fn main() {
-    let mut rng = rand::rng();
-    println!("Random die roll: {}", rng.random_range(1..=10));
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {error:?}"),
+    };
 }
