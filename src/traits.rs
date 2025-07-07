1890363fd715453262e2e511141f7c6abd36d007
Durga Prasad <durgaprasadachana1108@gmail.com>
2025-07-07 20:40:36 +0530

    Feat: traits learnt

--- src/traits.rs
+++ src/traits.rs
@@ -1,26 +1,61 @@
 trait Summary {
-    fn Summarize(&self) -> String {
-        return String::from("Summarize");
-    }
+    fn Summarize(&self) -> String;
 }
-
-struct User {
-    name: String,
-    age: u32,
+struct NewsArticle {
+    headline: String,
+    content: String,
+    author: String,
 }
-impl Summary for User {
-    fn Summarize(&self) -> String {
-        return format!("Nmae is {} and age is {}", self.name, self.age);
-    }
-}
-pub fn notify(u: &impl Summary) {
-    println!("{}", u.Summarize());
+struct Tweet {
+    username: String,
+    content: String,
+    reply: bool,
+    retweet: bool,
 }
 fn main() {
-    let user1 = User {
-        name: String::from("dp"),
-        age: 22,
+    let tweet = Tweet {
+        username: String::from("dp"),
+        retweet: false,
+        reply: false,
+        content: String::from("Hey There"),
     };
-    println!("{}", user1.Summarize());
-    notify(&user1);
+    let newsart = NewsArticle {
+        headline: String::from("asdf"),
+        content: String::from(""),
+        author: String::from("dp"),
+    };
+    news(tweet);
+    news(newsart)
+}
+// this is the shared behaviour
+impl Summary for Tweet {
+    fn Summarize(&self) -> String {
+        let content = format!("Tweet by :{}", self.username);
+        content
+    }
+}
+impl Summary for NewsArticle {
+    fn Summarize(&self) -> String {
+        let content = format!("News by :{}", self.author);
+        content
+    }
+}
+fn news(source: impl Summary) {
+    println!("There is a news in the market");
+    println!("{}", source.Summarize());
+}
+fn news_aggregator_tweet(tweet: Tweet) {
+    println!("There is a news in market");
+    println!(
+        "The news is that {} and is publilshed by {}",
+        tweet.content, tweet.username
+    );
+}
+
+fn news_aggregator_news(news: NewsArticle) {
+    println!("There is a news in market");
+    println!(
+        "The news is that {} and is publilshed by {}",
+        news.content, news.author
+    );
 }
